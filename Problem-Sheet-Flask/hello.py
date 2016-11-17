3d3da98af9efcc38e7402fbb69cb25999e2ee5fe
taraokelly <taraokelly2@gmail.com>
2016-11-17 11:39:31 +0000

    Forth Commit: multiple routing added

--- Problem-Sheet-Flask/hello.py
+++ Problem-Sheet-Flask/hello.py
@@ -7,5 +7,14 @@ app = Flask(__name__)
 @app.route("/")
 def root():
     return app.send_static_file('index.html')
+	
+@app.route('/name')
+def fname():
+	return 'Your name is'
+
+@app.route('/name/<name>')
+def finame(name):
+	return 'Your name is ' + name	
+	
 if __name__ == "__main__":
     app.run()
