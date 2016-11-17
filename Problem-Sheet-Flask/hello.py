901e1422dd2d85c5f1d78053ff85a53a24d34b1d
taraokelly <taraokelly2@gmail.com>
2016-11-17 11:16:01 +0000

    Third Commit: HTML with Bootstrap Template added

--- Problem-Sheet-Flask/hello.py
+++ Problem-Sheet-Flask/hello.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+#@app.route("/")
+#def hello():
+ #   return "Hello World!"
 @app.route("/")
-def hello():
-    return "Hello World!"
-
+def root():
+    return app.send_static_file('index.html')
 if __name__ == "__main__":
     app.run()
