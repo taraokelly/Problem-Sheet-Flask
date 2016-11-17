1a6af21759a43f7470f8ac06417dce7b913dfbb2
taraokelly <taraokelly2@gmail.com>
2016-11-17 16:41:49 +0000

    Fifth Commit: HTML form and GET method added

--- Problem-Sheet-Flask/hello.py
+++ Problem-Sheet-Flask/hello.py
@@ -1,20 +1,27 @@
-from flask import Flask
-app = Flask(__name__)
+#from flask import Flask
+import flask as fl
+app = fl.Flask(__name__)
 
-#@app.route("/")
-#def hello():
- #   return "Hello World!"
+
 @app.route("/")
 def root():
     return app.send_static_file('index.html')
-	
+
 @app.route('/name')
 def fname():
-	return 'Your name is'
+	return 'Your name is ' + fl.request.values["name"]
+	
+#@app.route("/")
+#def hello():
+#   return "Hello World!"
 
-@app.route('/name/<name>')
-def finame(name):
-	return 'Your name is ' + name	
+#@app.route('/name')
+#def fname():
+#	return 'Your name is'
+	
+#@app.route('/name/<name>')
+#def finame(name):
+#	return 'Your name is ' + name	
 	
 if __name__ == "__main__":
     app.run()
